1da19b27300aabdccc2773ebb9aafa416f464abd
michaelrushton-dev <michaelwjrushton@gmail.com>
2023-01-07 14:58:14 +0000

    abstracted the logic of postNewUser from 'routes' into 'funcs'

--- funcs/userFuncs.js
+++ funcs/userFuncs.js
@@ -10,3 +10,20 @@ export async function getAllUsers() {
         console.log(error);
     }
 }
+
+export async function postNewUser(req) {
+    try {
+        // creating a new db entry (document) with the model of "User"
+        const newUser = new Users({
+            name: req.body.name,
+            email: req.body.email,
+            shareLocation: req.body.shareLocation,
+        });
+        //asyncronously saving it to the database
+        await newUser.save();
+        console.log('saved new user!', newUser);
+    } catch (error) {
+        // code to handle the error
+        console.log(error);
+    }
+}
--- routes/users.js
+++ routes/users.js
@@ -1,31 +1,17 @@
 import express from 'express';
-import { getAllUsers } from '../funcs/userFuncs.js';
-import Users from '../models/Users.js';
+import { getAllUsers, postNewUser } from '../funcs/userFuncs.js';
 const router = express.Router();
 
-// Get all users - calls getAllUsers() from funcs
+// GET ALL USERS - calls getAllUsers() from funcs
 router.get('/', async (req, res) => {
     const result = await getAllUsers();
     return res.json({ success: true, payload: result });
 });
 
-//Post a new user
+//POST A NEW USER
 router.post('/', async (req, res) => {
     console.log(req.body);
-    try {
-        // creating a new db document with the model of "User"
-        const newUser = new Users({
-            name: req.body.name,
-            email: req.body.email,
-            shareLocation: req.body.shareLocation,
-        });
-        //asyncronously saving it to the database
-        await newUser.save();
-        console.log('saved new user!', newUser);
-    } catch (error) {
-        // code to handle the error
-        console.log(error);
-    }
+    postNewUser(req);
 });
 
 export default router;
