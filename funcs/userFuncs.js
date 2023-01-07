32428698b6d2aa11f6176b459bee4b6cb7983e8f
michaelrushton-dev <michaelwjrushton@gmail.com>
2023-01-07 14:38:57 +0000

    set up route for posting a new user model to DB and route for fetching all users from DB - all working so far

--- funcs/userFuncs.js
+++ funcs/userFuncs.js
@@ -1,8 +1,11 @@
+import Users from '../models/Users.js';
+
 export async function getAllUsers() {
     try {
-        const userFetch = await usersModel.find();
-        console.log('reponse');
+        const userFetch = await Users.find();
+        console.log('returning all users!');
         console.log(userFetch);
+        return userFetch;
     } catch (error) {
         console.log(error);
     }
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,31 @@
 import express from 'express';
 import { getAllUsers } from '../funcs/userFuncs.js';
+import Users from '../models/Users.js';
 const router = express.Router();
 
 // Get all users - calls getAllUsers() from funcs
 router.get('/', async (req, res) => {
     const result = await getAllUsers();
     return res.json({ success: true, payload: result });
+});
+
+//Post a new user
+router.post('/', async (req, res) => {
+    console.log(req.body);
+    try {
+        // creating a new db document with the model of "User"
+        const newUser = new Users({
+            name: req.body.name,
+            email: req.body.email,
+            shareLocation: req.body.shareLocation,
+        });
+        //asyncronously saving it to the database
+        await newUser.save();
+        console.log('saved new user!', newUser);
+    } catch (error) {
+        // code to handle the error
+        console.log(error);
+    }
 });
 
 export default router;
